7419051e197e0f2bf4cf483f7507bd354d046ce6
KhalidM53 <57159395+KhalidM53@users.noreply.github.com>
2020-02-29 09:51:53 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else{
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password:"",
-        database: "burgers_db" 
+        host: 'yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'k2tzba1sftx9ns63',
+        password:"d774jqp5a177xsu2",
+        database: "oslqtikjh0oo79u7" 
     });
-};
+// };
 
 connection.connect();
 module.exports = connection;
